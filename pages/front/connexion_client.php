74b0551666e7b7ad31a481a7f1f6ae22cbd217c0
theomanturan <theoman.turan@hotmail.fr>
2025-03-28 13:47:23 +0100

    form connexion client + css

---
+++ pages/front/connexion_client.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="/shoe_shop/css/style.css">
+</head>
+
+<body>
+  <section class="connexion_client_section">
+    <h1>Saisis ton identifiant ou ton adresse e-mail pour te connecter</h1>
+
+    <form class="connexion_client_form">
+      <div>
+        <input type="email" id="email" name="email" placeholder="Email" required>
+      </div>
+      <p>
+        En continuant, tu acceptes les <a href="#">conditions d’utilisation</a> et tu confirmes avoir lu la <a
+          href="#">politique de confidentialité</a>.
+      </p>
+      <button type="submit" class="connexion_client_button">Continuer</button>
+    </form>
+  </section>
+</body>
+
+</html>
