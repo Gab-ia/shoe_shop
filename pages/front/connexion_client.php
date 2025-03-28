1d35b7905560cd3343169d68bd2105038dd20f12
Gab-ia <deoliveiragabrielle88@gmail.com>
2025-03-28 14:41:40 +0100

    fix link

--- pages/front/connexion_client.php
+++ pages/front/connexion_client.php
@@ -5,13 +5,13 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
-  <link rel="stylesheet" href="/shoe_shop/css/style.css">
+  <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
 
 <body>
   <section class="connexion_client_section">
-    <img src="/shoe_shop/img/logo-nike.svg" alt="">
+    <img src="/img/logo-nike.svg" alt="">
 
     <h1>Saisis ton identifiant ou ton adresse e-mail et ton mot de passe pour te connecter</h1>
 
@@ -35,7 +35,7 @@
     </form>
   </section>
 
-  <script src="/shoe_shop/js/script_password.js"></script>
+  <script src="/js/script_password.js"></script>
 </body>
 
 </html>
