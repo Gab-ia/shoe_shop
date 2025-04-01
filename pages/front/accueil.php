100258a0b06264259cc0950bdd2d3f498c1a0a2e
Gab-ia <deoliveiragabrielle88@gmail.com>
2025-04-01 15:36:52 +0200

    chgmt chemins

--- pages/front/accueil.php
+++ pages/front/accueil.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>shoe_shop</title>
-    <link rel="stylesheet" href="/shoe_shop/css/style2.css">
+    <link rel="stylesheet" href="/css/style2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script type="text/javascript" src='/shoe_shop/js/accueil.js'> </script>
+    <script type="text/javascript" src='/js/accueil.js'> </script>
 
 </head>
 
