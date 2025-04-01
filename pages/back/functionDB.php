4ae52ba8ea14da715df371667085701ce4e6ac23
Gab-ia <deoliveiragabrielle88@gmail.com>
2025-04-01 09:59:54 +0200

    fcvgb

--- pages/back/functionDB.php
+++ pages/back/functionDB.php
@@ -12,13 +12,15 @@ function getAllShoes($db) {
 
 function createShoes ($db, $id, $nom, $prix, $marque, $taille, $genre, $descript, $img) {
     try {
-        $insert = $db->prepare('INSERT INTO chaussure SET prenom = :prenom, nom = :nom, prix = :prix, marque = :marque, taille = :taille, genre = :genre, descript = :descript, img = :img');
-        $insert->bindValue(':prenom',trim(htmlspecialchars($first_name)), PDO::PARAM_STR);
-        $insert->bindValue(':nom',trim(htmlspecialchars($last_name)), PDO::PARAM_STR);
-        $insert->bindValue(':prix',trim(htmlspecialchars($last_name)), PDO::PARAM_STR);
-        $insert->bindValue(':marque',trim(htmlspecialchars($last_name)), PDO::PARAM_STR);
-        $insert->bindValue(':taille',trim(htmlspecialchars($last_name)), PDO::PARAM_STR);
-        $insert->bindValue(':genre',trim(htmlspecialchars($department)), PDO::PARAM_INT);
+        $insert = $db->prepare('INSERT INTO chaussure SET prenom = :prenom, nom = :nom, prix = :prix, marque = :marque, taille = :taille, genre = :genre, descript = :descript, img = :img, id = :id');
+        $insert->bindValue(':prenom',trim(htmlspecialchars($prenom)), PDO::PARAM_STR);
+        $insert->bindValue(':nom',trim(htmlspecialchars($nom)), PDO::PARAM_STR);
+        $insert->bindValue(':prix',trim(htmlspecialchars($prix)), PDO::PARAM_FLOAT);
+        $insert->bindValue(':marque',trim(htmlspecialchars($marque)), PDO::PARAM_STR);
+        $insert->bindValue(':taille',trim(htmlspecialchars($taille)), PDO::PARAM_INT);
+        $insert->bindValue(':genre',trim(htmlspecialchars($genre)), PDO::PARAM_STR);
+        $insert->bindValue(':img',trim(htmlspecialchars($img)), PDO::PARAM_STR);
+        $insert->bindValue(':id',trim(htmlspecialchars($id)), PDO::PARAM_INT);
         $insert->execute();
         $insert_id = $db->lastInsertId();
         return $db->lastInsertId();
