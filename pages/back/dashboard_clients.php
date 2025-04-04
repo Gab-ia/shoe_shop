49247e39daf90e0aa116aad5c11e2a4356fe6c2d
Gab-ia <deoliveiragabrielle88@gmail.com>
2025-04-04 14:57:08 +0200

    dashboard shoes + clients fini

--- pages/back/dashboard_clients.php
+++ pages/back/dashboard_clients.php
@@ -12,6 +12,17 @@
         header("Location: dashboard_clients.php");
         exit();
     }
+
+    $currentSort = $_GET['sort'] ?? '';
+    $currentOrder = $_GET['order'] ?? 'asc';
+
+    $getClients = getAllClients($db, $currentSort, $currentOrder);
+
+    function getSortLink($field, $currentSort, $currentOrder) {
+        $newOrder = ($currentSort === $field && $currentOrder === 'asc') ? 'desc' : 'asc';
+        return "?sort=$field&order=$newOrder";
+    }
+
 ?>
 
 <!DOCTYPE html>
@@ -66,6 +77,30 @@
             </div>
 
             <div id="list_client" class="list_main">
+
+                <div class="list_item_filter">
+                    <?php
+                        $fields = [
+                            'nom' => 'Nom',
+                            'prenom' => 'Prénom',
+                            'mail' => 'Mail',
+                            'identifiant' => 'Identifiant',
+                            'id' => 'Date de création de compte'
+                        ];
+
+                        foreach ($fields as $field => $label) {
+                            $link = getSortLink($field, $currentSort, $currentOrder);
+                            $isActive = ($currentSort === $field) ? 'list_icon_filter_active' : '';
+                            echo '<a class="size6 list_info_filter" href="' . $link . '">';
+                            echo $label;
+                            echo '<svg class="list_icon_filter ' . $isActive . '" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">';
+                            echo '<path d="M137.4 41.4c12.5-12.5 32.8-12.5 45.3 0l128 128c9.2 9.2 11.9 22.9 6.9 34.9s-16.6 19.8-29.6 19.8L32 224c-12.9 0-24.6-7.8-29.6-19.8s-2.2-25.7 6.9-34.9l128-128zm0 429.3l-128-128c-9.2-9.2-11.9-22.9-6.9-34.9s16.6-19.8 29.6-19.8l256 0c12.9 0 24.6 7.8 29.6 19.8s2.2 25.7-6.9 34.9l-128 128c-12.5 12.5-32.8 12.5-45.3 0z"/>';
+                            echo '</svg>';
+                            echo '</a>';
+                        }
+                    ?>
+                </div>
+
                 <?php 
                     foreach($getClients as $clients) {
                         echo '<div class="list_item">
@@ -73,18 +108,18 @@
                                 <p class="size6 list_info">'. $clients['prenom'] . '</p>
                                 <p class="size6 list_info">'. $clients['mail'] . '</p>
                                 <p class="size6 list_info">'. $clients['identifiant'] . '</p>
-                                <a  class=" list_delete_btn" onclick="showModal(\'modal-' . $clients['id'] . '\')"><svg class="list_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>Supprimer</a>
-                            </div>
-                            <div id="modal-' . $clients['id'] . '" class="modal" >
+                                <a  class="list_delete_btn" onclick="showModal(\'modal-' . $clients['id'] . '\')"><svg class="list_icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0L284.2 0c12.1 0 23.2 6.8 28.6 17.7L320 32l96 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 96C14.3 96 0 81.7 0 64S14.3 32 32 32l96 0 7.2-14.3zM32 128l384 0 0 320c0 35.3-28.7 64-64 64L96 512c-35.3 0-64-28.7-64-64l0-320zm96 64c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16l0 224c0 8.8 7.2 16 16 16s16-7.2 16-16l0-224c0-8.8-7.2-16-16-16z"/></svg>Supprimer</a>
+                                <div id="modal-' . $clients['id'] . '" class="modal" >
                                 <div class="modal-content">
-                                    <div class="modaltop">
-                                        <span class="close" onclick="closeModal(\'modal-' . $clients['id'] . '\')">&times;</span>
-                                        <h2>Confirmation</h2>
-                                    </div>
-                                    <p class="modaltxt" id="modalText">Êtes-vous sûr de vouloir supprimer le compte de ' . $clients["nom"] . ' ' . $clients['prenom'] . '?</p>
-                                    <div class="modalconfirm">
-                                        <a id="" href="dashboard_clients.php?action=Supprimer&id=' . $clients['id'] . '" class="list_delete_btn">Supprimer</a>&nbsp;
-                                        <a class="list_cancel_btn" onclick="closeModal(\'modal-' . $clients['id'] . '\')">Annuler</a>
+                                        <div class="modaltop">
+                                            <span class="close" onclick="closeModal(\'modal-' . $clients['id'] . '\')">&times;</span>
+                                            <h2>Confirmation</h2>
+                                        </div>
+                                        <p class="modaltxt" id="modalText">Êtes-vous sûr de vouloir supprimer le compte de ' . $clients["nom"] . ' ' . $clients['prenom'] . '?</p>
+                                        <div class="modalconfirm">
+                                            <a id="" href="dashboard_clients.php?action=Supprimer&id=' . $clients['id'] . '" class="list_delete_btn">Supprimer</a>&nbsp;
+                                            <a class="list_cancel_btn" onclick="closeModal(\'modal-' . $clients['id'] . '\')">Annuler</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>';
@@ -107,21 +142,6 @@
                 </div>
             
 -->
-
-<!--
-            <div class="modal-content">
-                <div class="modaltop">
-                    <span class="close" onclick="closeModal(\'modal-' . $department['id'] . '\')">&times;</span>
-                    <h2>Confirmation</h2>
-                </div>
-                <p class="modaltxt" id="modalText">Êtes-vous sûr de vouloir supprimer le département ' . $department["name"] . '?</p>
-                <div class="modalconfirm">
-                    <a id="" href="services.php?action=supprimer&id=' . $department['id'] . '" class="btn delete">Supprimer</a>&nbsp;
-                    <a class="btn btn-secondary" onclick="closeModal(\'modal-' . $department['id'] . '\')">Annuler</a>
-                </div>
-            </div>
--->
-
 
         </section>
     </main>
