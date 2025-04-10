210cad41305d594a6a67a41a2184c56454a478e0
Samirbelabed <lesam88@hotmail.fr>
2025-04-10 11:59:06 +0000

    accueil10/04

---
+++ pages/front/accueil.js
@@ -0,0 +1,25 @@
+let image = document.getElementById('img');
+
+
+image.addEventListener ('mouseover', () => {
+
+
+image.style.opacity = '0.5';
+
+
+
+
+
+});
+
+
+image.addEventListener ('mouseout', () => {
+
+
+    image.style.opacity = '1';
+    
+    
+    
+    
+    
+    });
