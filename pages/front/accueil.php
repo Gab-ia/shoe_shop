40eda2471db9efdf862789b2eb52d9fcb9044b50
Samirbelabed <lesam88@hotmail.fr>
2025-04-14 07:29:00 +0000

    accueil 14/04

--- pages/front/accueil.php
+++ pages/front/accueil.php
@@ -21,8 +21,6 @@ try {
 
 
 
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -136,6 +134,8 @@ in their exact original form, accompanied by English versions from the 1914 tran
 
 <?php
 
+
+
 $sql = "SELECT * FROM shoes";
 $stmt = $db->query($sql);
 
@@ -146,13 +146,13 @@ while ($row = $stmt->fetch()){
             
     <ul>
     
-    <li style ="color: var(--couleur-4);  background: linear-gradient(0deg, white 85%, var(--couleur-3) 15% );  
+    <li id ="chaussure" style ="color: var(--couleur-4);  background: linear-gradient(0deg, white 85%, var(--couleur-3) 15% );  
     padding-top: 10px; padding-bottom: 10px; box-shadow: 5px 5px 5px black; width: 300px; height:450px; font-size:21px;">
     <strong style="font-style: italic;"> '. $row['marque'] .' </strong> '. $row['nom'] .' &nbsp; 
     <strong style= "color: var(--couleur-1); float: right;"> '. $row['taille'] .' </strong> <br> <br>
-    <img src="/img/shoes/'. $row['image'] .'" style= "width: 420px; height: 260px; margin: 40px -70px;" id ="img" alt=""/> 
-    <br> <p style= "color: var(--couleur-1); margin: -35px 0; background-color: yellow; padding: 10px 10px; width: 110px; 
-    font-size: 30px;"> '. $row['prix'] .' <i class="fa-solid fa-euro-sign"></i> </p>
+    <img id = "img" src="/img/shoes/'. $row['image'] .'" style= "width: 420px; height: 260px; margin: 40px -70px;" id ="img" alt=""/> 
+    <br> <p id ="savoir" style= "position: absolute; margin: 50% auto; color: black;"> En savoir plus </p> <p id= "prix" style= "color: var(--couleur-1); margin: -35px 0; background-color: yellow; padding: 10px 10px; width: 110px; 
+    font-size: 30px;"> '. $row['prix'] .' <i id ="euro" class="fa-solid fa-euro-sign"></i> </p>
     </li> </br>
     
     </ul> ';
@@ -187,100 +187,6 @@ while ($row = $stmt->fetch()){
 
 
 
-$genre_by = "genre";
-$gd = "ASC";
-
-
-if (isset($_GET["genre_by"]) && ($_GET["genre_by"] == "genre") ){
-
-    $genre_by = $_GET["genre_by"];
-
-    }
-
-
-    if (isset($_GET["gd"]) && ($_GET["gd"] == "ASC" || $_GET["gd"] == "DESC")){
-
-        $gd = $_GET["gd"];
-
-        $sql = "SELECT * FROM shoes WHERE $genre_by LIKE 'Homme%' ORDER BY taille $gd ";
-    
-    } 
-
-
-
-
-
-
-$size_by = "taille";
-$sm = "ASC";
-
-
-
-if (isset($_GET["size_by"]) && ($_GET["size_by"] == "taille") ){
-
-    $size_by = $_GET["size_by"];
-
-    }
-
-
-    if (isset($_GET["sm"]) && ($_GET["sm"] == "ASC" || $_GET["sm"] == "DESC")){
-
-        $sm = $_GET["sm"];
-
-        $sql = "SELECT * FROM shoes WHERE $size_by BETWEEN 32 AND 36 ORDER BY $size_by $sm";
-    
-    } 
-
-    
-    
-    
-    
-  
-    
-$date_by = "id";
-$new = "ASC";
-
-
-if (isset($_GET["date_by"]) && ($_GET["date_by"] == "id") ){
-
-    $date_by = $_GET["date_by"];
-
-    }
-
-
-    if (isset($_GET["new"]) && ($_GET["new"] == "ASC" || $_GET["new"] == "DESC")){
-
-        $new = $_GET["new"];
-
-        $sql = "SELECT * FROM shoes ORDER BY shoes.$date_by $new";
-    
-    } 
-
-    
-
-
-
-$sort_by = "prix";
-$order = "ASC";
-    
-    
-if (isset($_GET["sort_by"]) && ($_GET["sort_by"] == "prix") ){
-    
-    
-        $sort_by = $_GET["sort_by"];
-    
-    
-     }
-    
-    
-     if (isset($_GET["order"]) && ($_GET["order"] == "ASC" || $_GET["order"] == "DESC")){
-    
-        $order = $_GET["order"];
-      
-        $sql = "SELECT * FROM shoes WHERE $sort_by ORDER BY $order";
-    
-    
-    } 
     
    
     
